f9815ec000354be791f4d97fb26c9705cc988394
Muhriz <muhrizmadathil@gmail.com>
2021-09-08 13:19:36 +0530

    user js updated

--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.send('admin page')
-  res.render('index')
+  res.send('admin page')
+  
 });
 
 module.exports = router;
