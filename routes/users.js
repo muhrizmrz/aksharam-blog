4aa0123b2cba432951182897a0ea0156a32534fa
Muhriz <muhrizmadathil@gmail.com>
2021-09-23 16:55:07 +0530

    almost finished...some finishing work

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var article_helper = require('../helpers/add-article')
+var article_helper = require('../helpers/article_models')
+const collection = require('../confiq/collection');
+const db = require('../confiq/connection')
+const objectId = require('mongodb').ObjectId
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  article_helper.getRecentArticles().then((result)=>{
-    console.log(result)
-    res.render('index',{recentArticles:result})
+router.get('/', async(req, res)=> {
+  let articles = await db.get().collection(collection.ARTICLE_COLLECTION).find().sort({date:-1}).limit(4).toArray()
+  let filterArticles = await db.get().collection(collection.ARTICLE_COLLECTION).find().limit(6).toArray()
+  article_helper.getRecentArticles().then((result)=>{  
+    res.render('index',{allArticles:articles,recentArticles:result,filterArticles:filterArticles})
   })
-  
 });
+
+/* GET view article */
+router.get('/:id',async(req,res)=>{
+  var articleToBeView = await db.get().collection(collection.ARTICLE_COLLECTION).findOne({_id:objectId(req.params.id)})
+  let sameTagArticles = await db.get().collection(collection.ARTICLE_COLLECTION).find({catagory:articleToBeView.catagory}).toArray()
+  res.render('view-article',{article:articleToBeView,sameTagArticles:sameTagArticles})
+})
 
 module.exports = router;
